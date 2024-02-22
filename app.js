2dee8d176c87a739942de79f9f89338ba592f807
Coxosvaldo <ghenriquet2139@gmail.com>
2024-02-22 19:35:02 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,53 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    
+
+    if (de >= ate) {
+        alert('Erro 1 - Vai travar se fizer isso!!');
+        return;
+    }
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = numeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = numeroAleatorio(de, ate);
+        }
+
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    BotaoDesativado();
+}
+
+function numeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function BotaoDesativado() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    BotaoDesativado();
+}
+
